10d4229f1947d63ea5eb2af675d6e26b7aa8ce4b
SpecialFriendRice <corinneborgeaud@hotmail.com>
2024-03-02 19:21:07 +0000

    save button event local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
 //Use event.preventDefault() inside functions and standard js/jQuery wrapper learnt last week?
-//starter code has included links to Bootstrap, GoogleAPIs and FontAwesome (presumably for delete button icon) and scripts for jQuery and DayJS
 //<p> div already provided in header for inserting today's date. I've currently hardcoded text "Today is" into html but should I dynamically do this as well as DayJS ref?
 //"timeblocks" = grid for working day (are we allowed to use a Bootstrap TABLE template?) - CSS has the styles for these elements already defined!
 //is there any point in creating divs with hours of day dynamically? Surely set times e.g. 9am to 6pm is best? Should I create a for loop to create a row for each hour between start of day and end of day or can I hardcode this?
@@ -30,19 +29,38 @@ var tbodyEl = $('.tBody');
 var today = dayjs();
 currentDayEl.text(today.format("D MMM YYYY"));
 
-//CURRENT HOUR TO COMPARE WITH TIMEBLOCK
+//CURRENT HOUR TO COMPARE WITH TIMEBLOCK - class past/present/future is now attribute of timeblock text area, will this have the 
 var currentHr = dayjs().hour()
 
-//WHY IS EVENT LISTENER NOT WORKING? "saveBtnEl.addEventListener is not a function"
-// saveBtnEl.addEventListener("click", function(event) {
-//     alert("saveBtn works");
-// });
+//FINISH Colour change function depending on current time; need to name input fields
+// function colorChange () {
+//     if (ALONG THE LINES OF CURRENTHOUR>HOUR) {
+//         $('#textarea'). //change class from past to present etc
+// } else if () {
+    
+// }
+// }
+
+
+// WHY IS EVENT LISTENER NOT WORKING? "saveBtnEl.addEventListener is not a function" - because saveBtnEl is a jQuery object and addEventListener works on DOM objects
+saveBtnEl.on("click", function(event) {
+    alert("save button works");
+    //do i need a preventDefault?
+    event.preventDefault(); 
+    //STILL TO DO - SAVE INPUT IN TEXTAREA TO LOCAL STORAGE WHEN SAVE BUTTON IS CLICKED
+    //NOTE THAT AS ALL ROWS (HOURS) HAVE THE SAME ATTRIBUTE FOR THIS SECTION: ID = textarea, need to focus on the user input to the textarea in the same row i.e. a sibling of the save button that was clicked
+    var eventEl = $(this).siblings("textarea").value;
+    localStorage.setItem("eventEl", eventEl);
+
+});
+
 
 
 
 
 
 //save to local storage function needed NEED TO NAME INPUT FOR EVENT USER INPUT - BELOW I HAVE ASSIGNED IT THE ID eventInput
+
 // saveBtnEl.addEventListener("click", function(event) {
 //     event.preventDefault();
   
@@ -50,13 +68,9 @@ var currentHr = dayjs().hour()
   
 //       localStorage.setItem("eventEl", eventEl);
       
-//       //HAVEN'T DEAL WITH CLEARING OUT INPUT ETC WITH METHODS LIKE THE BELOW
+//       //HAVEN'T DEALT WITH CLEARING OUT INPUT ETC WITH METHODS LIKE THE BELOW
 //       renderLastRegistered();
 //     }
 
-//FINISH Colour change function depending on current time; need to name input fields
-// function colorChange () {
-//     if ()
-// }
 
 
