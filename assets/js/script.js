a5fc95473bbbba7b8fa738920844f2ac71ff633a
SpecialFriendRice <corinneborgeaud@hotmail.com>
2024-03-04 10:29:10 +0000

    DOM traversal and parent elements code

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,10 @@ $(document).ready(function() {
    
 //VARIABLES
 
-var containerEl = $('.container');
+var containerEl = $('.container'); //do I need this?
 var currentDayEl = $('#currentDay');
 var saveBtnEl = $('.saveBtn');
-var tbodyEl = $('.tBody'); 
+var tbodyEl = $('.tBody'); //do I need this?
 
 //CURRENT DATE TO DISPLAY IN HEADER
 
@@ -18,6 +18,7 @@ var currentHr = dayjs().hour();
 
 //To get the hour value already hard coded into each html row to compare with actual time:
 //first get all elements with class = hour 
+//in non-jQuery:
 //var hourEl = document.querySelectorAll('.hour');
 //get the hour (0-24) by trimming the hardcoded hour text inside
 //var hourValue = hourEl.textContent.trim();
@@ -25,21 +26,34 @@ var currentHr = dayjs().hour();
 //OR 
 //Use jQuery $.each() to iterate through the jQuery object?
 //For each element with a class of hour, get the hourValue from the text hardcoded within it
-//NB why is there an error for the dot to signify class in $(.'hour') below?
+//NB why was there an error for the dot to signify class in $(.'hour') below?
 //$(.'hour').each(function() {
-$('hour').each(function() {
+$('.hour').each(function() {
     var hourValue = parseInt($(this).text().trim().split(':')[0]);
-// the code below picks up the textarea which is the sibling to the element which holds the hour value; the textarea is what we want to change colour
-    var textarea = $(this).siblings('textarea'); 
+
+//The DOM traversal that worked with the textareaValue works here too, to pick the right element (doesn't seem to work with sibling term in relation to the hour element) and get it to change background colour by using starter CSS and addClass: 
+
+    var textarea = $(this).parent().prev().find('textarea');
+    //console.log(textarea);
+
 //is addClass the correct method?
     if (hourValue < currentHr) {
         textarea.addClass('past');
-       } else if (hourValue > currentHr) {
-        textarea.addClass('future');
-       } else {
+    } else if (hourValue === currentHr) {
         textarea.addClass('present');
+    } else if (hourValue > currentHr) {
+     textarea.addClass('future');
+       } else {
+        //the present class was originally in as the else but neither works
        }
+
+       //TESTS
+       //Both currentHr and hourValue are number type
+       //console.log("Current hour is " + currentHr);
+       //console.log(hourValue);
+      
 });
+
 
 //or do I need a for loop to go through 09:00 to end of day? Something like
 // for (let i = 9; i < 18; i++) {
@@ -53,29 +67,33 @@ $('hour').each(function() {
 // }
 
 //Save input from textarea to local storage on clicking of the save button
-// WHY WAS EVENT LISTENER NOT WORKING? "saveBtnEl.addEventListener is not a function" - because saveBtnEl is a jQuery object and addEventListener works on DOM objects
 saveBtnEl.on('click', function(event) {
     event.preventDefault(); 
     //alert("save button works");
     //localStorage.setItem("Your Local Storage", "Is Working");
     
-    //Harness the fact that the user input is to the textarea that is a sibling of the save button.
-    //the below is just bringing back eventEl as undefined; 
-    //DEBUG **this is because currently $(this).siblings('textarea').val() gives an empty object (array?)**
-    var eventEl = $(this).siblings('textarea').val();
+    //Harness the fact that the user input is to the textarea that is a relation of the save button's parent element.
+    const textareaValue = $(this).parent().prev().find('textarea').val();
+    //console.log(textareaValue);
+
+    //BELOW IS WAS WHAT WASN'T WORKING!
+    //var textareaValue = $(this).siblings('textarea').val();
     //console.log($(this).siblings('textarea'));
     //console.log($(this).siblings('textarea').length); 
+    //console.log($(this)); returns the button element as it should
 
-    //Will val() work for this string of data? Does it need to be trimmed?
+
+    //DEBUG local storage is now working, but text is not persisting on refresh page
 
     //To distinguish each textarea input so they don't write over each other in local storage, need to set each a unique key - in this case the unique id of the textarea element. Was there a way to harness the hourValue coded above and use that as the key that would have made this simpler?
-    var key = $(this).siblings('textarea').attr('id');
-    localStorage.setItem(key, eventEl);
+    var key = $(this).parent().prev().find('textarea').attr('id');
+    localStorage.setItem(key, textareaValue);
 
 });
 
 //clearing out local storage/overwriting renderLastRegistered() etc?
 //If time, put character limit on event text and/or word wrap?
+//Trim textareaValue before saving to local storage?
 
 });
 
